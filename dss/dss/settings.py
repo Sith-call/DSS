46a499135f6f0466472cf8a6c6fc163dea9b791f
WooSeok Choe <57928967+Sith-call@users.noreply.github.com>
2022-09-22 09:33:33 +0900

    Update settings.py

--- dss/dss/settings.py
+++ dss/dss/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secret_key.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
